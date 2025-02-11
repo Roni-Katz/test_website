34636c405f5cd689e19960f6f50b0fc396f28d10
Roni Eisenberg <ronimkatz@gmail.com>
2025-02-11 14:54:01 +0200

    new

---
+++ config.js
@@ -0,0 +1,2 @@
+angular.module('backend.config', [])
+  .constant('SERVER_URL', 'https://api.creativeforagingtask.com/v1/');
--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("colorButton").addEventListener("click", function() {
-    const colors = ["#FF5733", "#33FF57", "#5733FF", "#FF33A6", "#33A6FF"];
-    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-});
